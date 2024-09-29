675084a53346ae4ea9bacd2136c5514003b27462
Natassa <112552395+natko22@users.noreply.github.com>
2024-09-29 15:28:40 +0200

    using dynamic server routes

--- pages/about.vue
+++ pages/about.vue
@@ -2,17 +2,19 @@
   <div>
     <h2>About</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem vitae
-      quibusdam numquam delectus veritatis quasi culpa. Dolore culpa atque vero
-      veniam excepturi labore adipisci accusantium obcaecati tenetur. Excepturi,
-      exercitationem consequatur!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aperiam
+      minima unde nemo harum quam maxime optio quos corrupti. Eum sapiente
+      facere nemo, laborum ullam non cum fuga quas eveniet harum molestiae minus
+      atque vel ratione illo quia, iure commodi dicta porro excepturi quam.
+      Facere, commodi corrupti ipsam neque totam reprehenderit laboriosam
+      laborum veniam est quo, repellat saepe impedit labore?
     </p>
+    <p>api response:</p>
+    <div>{{ data }}</div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
-
-<style>
+<style scoped>
 h2 {
   margin-bottom: 20px;
   font-size: 36px;
@@ -21,3 +23,15 @@ p {
   margin: 20px 0;
 }
 </style>
+
+<script setup>
+// const { data } = await useFetch('/api/ninja?name=mario')
+// const { data } = await useFetch('/api/ninja?name=mario', {
+//   method: 'post',
+//   body: {age: 30}
+// })
+
+// const { data } = await useFetch('/api/ninja')
+
+const { data } = await useFetch("/api/currency/GBP");
+</script>
