195c6be10f3954bf53e9b001138fca9a9963c0c7
Natassa <112552395+natko22@users.noreply.github.com>
2024-09-29 09:56:53 +0200

    add pages

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem vitae
+      quibusdam numquam delectus veritatis quasi culpa. Dolore culpa atque vero
+      veniam excepturi labore adipisci accusantium obcaecati tenetur. Excepturi,
+      exercitationem consequatur!
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
