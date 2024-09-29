6d427709b55e80be635ddbcf3aaa87fd270c3c11
Natassa <112552395+natko22@users.noreply.github.com>
2024-09-29 10:08:21 +0200

    create subfolders-learn routing

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere vero esse
+      similique reprehenderit pariatur, perspiciatis deserunt natus praesentium
+      et expedita libero iusto laborum quaerat officiis ab quis nulla quibusdam
+      alias.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { id } = useRoute().params;
+</script>
+
+<style></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Products</p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
